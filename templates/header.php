a9bc33bb7f6e2ecbf538af9c28f800781080cdf5
AlvBou <34zdraw@gmail.com>
2025-05-28 10:03:12 +0200

    28-05 Initialisation

---
+++ templates/header.php
@@ -0,0 +1,57 @@
+<header class="header">
+        <div class="logo">
+            <img src="<?php echo img_dir; ?>logoabcreation.png" alt="logo AB créations" height="50" width="50" />
+        </div>
+
+        <nav class="main-nav" role="navigation">
+            <div class="main-nav__wrapper">
+                <div class="main-nav_header">
+                    <button class="closeMainNav">❌</button>
+                </div>
+                    <ul>
+                    <li>
+                        <a href="index.html" class="selected" title="Accueil">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Accueil</font>
+                        </font>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="portfolio.html" title="Portefeuille">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Portefeuille</font>
+                        </font>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="a-propos.html" title="À propos de">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">À propos de</font>
+                        </font>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="contact.html" title="Contact">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Contact</font>
+                        </font>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="profil.html" title="Espace membre">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Espace membre</font>
+                        </font>
+                        </a>
+                    </li>
+                </ul>
+                <div class="main-nav_footer">
+                    <ul>
+                        <li><a href="page-connexion.html" class="btn btn-login">Se connecter</a></li>
+                        <li><a href="page-creationdecompte.html" class="btn btn-signup">S'inscrire</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <button class="openMainNav">☰</button>
+    </header>
